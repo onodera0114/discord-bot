8c7723cbddd5a27c3112b8f51e59459951187f31
onodera0114 <25942571+onodera0114@users.noreply.github.com>
2025-10-15 20:27:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ for (const file of commandFiles) {
 }
 
 client.once(Events.ClientReady, async () => {
-  client.user.setActivity('昼飯乃流儀', { type: 4 })
+  client.user.setActivity('領域展開 昼飯乃流儀', { type: 4 })
   const data = [];
   for (const commandName in commands) {
       data.push(commands[commandName].data)
